23f0a55a2064b1d0e382ab2542a824fd62486f86
陈云 <1004183222@qq.com>
2015-12-03 10:45:02 +0800

    第一版：从模板生成HTML

---
+++ src/js/page/index.js
@@ -0,0 +1,6 @@
+//引入CSS
+require("../../css/lib/reset.css");
+require("../../css/common/global.css");
+require("../../css/page/index.css");
+
+document.write('Hello Index Js');
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path=require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports={
+	entry:{
+    		index:"./src/js/page/index.js",
+    	},
+    output:{
+        path: path.join(__dirname,'dist'),
+        publicPath: "/webpack/dist/",
+        filename: "js/[name].js",
+        chunkFilename: "js/[id].chunk.js"
+    },
+    module: {
+        loaders: [	//加载器
+            {test: /\.css$/, loader:ExtractTextPlugin.extract("style", "css") }
+        ]
+    },
+    plugins:[
+    	new ExtractTextPlugin("css/[name].css"),	//单独使用style标签加载css并设置其路径
+    	new HtmlWebpackPlugin({						//根据模板插入css/js等生成最终HTML
+    		favicon:'./src/img/favicon.ico', //favicon路径
+			filename:'/view/index.html',	//生成的html存放路径，相对于 path
+			template:'./src/view/index.html',	//html模板路径
+			inject:true,	//允许插件修改哪些内容，包括head与body
+			hash:true	//为静态资源生成hash值
+		})
+    ]
+};
