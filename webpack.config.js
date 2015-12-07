0d4ef05245252ebe0fb11470dcb1aa0855ab4b94
陈云 <1004183222@qq.com>
2015-12-07 11:57:06 +0800

    代码压缩

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,12 @@ module.exports={
             $: 'jquery'
         }),
     	new ExtractTextPlugin("css/[name].css"),	//单独使用style标签加载css并设置其路径
+    	new webpack.optimize.UglifyJsPlugin({	//压缩代码
+		    compress: {
+		        warnings: false
+		    },
+		    except: ['$super', '$', 'exports', 'require']	//排除关键字
+		}),
     	new HtmlWebpackPlugin({						//根据模板插入css/js等生成最终HTML
     		favicon:'./src/img/favicon.ico', //favicon路径
 			filename:'/view/index.html',	//生成的html存放路径，相对于 path
@@ -32,8 +38,11 @@ module.exports={
 			hash:true,	//为静态资源生成hash值
 			minify:{	//压缩HTML文件
 				removeComments:true,	//移除HTML中的注释
-				collapseWhitespace:false	//删除空白符与换行符
+				collapseWhitespace:true	//删除空白符与换行符
 			}
 		})
-    ]
+    ],
+    devServer:{
+    	contentBase:'./dist/view'
+    }
 };
